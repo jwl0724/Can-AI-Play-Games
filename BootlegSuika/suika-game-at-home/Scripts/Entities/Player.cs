2e4c52481b59483148bc9a8552bd4dc8a5a7bd2f
Jonathan <122506441+jwl0724@users.noreply.github.com>
2025-03-12 11:16:13 -0700

    add extra functions

--- BootlegSuika/suika-game-at-home/Scripts/Entities/Player.cs
+++ BootlegSuika/suika-game-at-home/Scripts/Entities/Player.cs
@@ -3,8 +3,14 @@ using System;
 
 public partial class Player : Node2D
 {
+    // Constants
+    private Vector2 startPosition;
+
     // Components
     private Node2D fruitPosition;
+
+    // Running variables
+    [Export] public float Speed { get; private set; } = 200;
 
     // Signals
     [Signal] public delegate void DropFruitEventHandler();
@@ -15,11 +21,30 @@ public partial class Player : Node2D
     public override void _Ready()
     {
         fruitPosition = GetNode<Node2D>("FruitPosition");
+        startPosition = Position;
+
+        GameLoop.Instance.SetComponent(this);
     }
 
     public override void _Process(double delta)
     {
         if (heldFruit != null) heldFruit.GlobalPosition = fruitPosition.GlobalPosition;
+    }
+
+    public void Reset()
+    {
+        heldFruit = null;
+        Position = startPosition;
+    }
+
+    public void Move(float direction, double delta)
+    {
+        Position += Vector2.Right * direction * Speed * (float) delta;
+    }
+
+    public void DropHeldFruit()
+    {
+        EmitSignal(SignalName.DropFruit);
     }
 
     public void GiveFruit(Fruit fruit)
