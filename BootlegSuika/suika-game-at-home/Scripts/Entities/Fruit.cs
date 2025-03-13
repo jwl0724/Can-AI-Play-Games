3b10dcaead3c89ba179654f460cc152e77c3c9e6
jwl0724 <122506441+jwl0724@users.noreply.github.com>
2025-03-12 18:45:39 -0700

    implement physics logic and fusion logic

--- BootlegSuika/suika-game-at-home/Scripts/Entities/Fruit.cs
+++ BootlegSuika/suika-game-at-home/Scripts/Entities/Fruit.cs
@@ -8,6 +8,9 @@ public partial class Fruit : RigidBody2D
 	private CollisionShape2D body;
 	private Sprite2D sprite;
 
+	// Signals
+	[Signal] public delegate void FirstCollisionEventHandler();
+
 	// Running variables
 	public FruitType Type { get; private set; }
 	public bool Collided { get; private set; } = false;
@@ -15,14 +18,36 @@ public partial class Fruit : RigidBody2D
 
 	public override void _Ready()
 	{
-		// Setup collision body
+		Sleeping = true;
+		Freeze = true;
 		body = GetNode<CollisionShape2D>("Body");
-
-		// Setup sprite
 		sprite = GetNode<Sprite2D>("Sprite");
+
+		// Allow collision reporting
+		ContactMonitor = true;
+		MaxContactsReported = 8;
 	}
 
-	public void SetAttributes(FruitBuilder builder, FruitType type, CompressedTexture2D sprite, int scoreAmount, float radius)
+    public override void _PhysicsProcess(double delta)
+    {
+		if (IsQueuedForDeletion()) return; // In case another fruit called queue free for the fruit
+
+		// Spawn the next fruit on first collision
+		if (Collided == false && GetContactCount() != 0)
+		{
+			Collided = true;
+			EmitSignal(SignalName.FirstCollision);
+		}
+		// Handle fruit fusion
+		foreach(Node2D body in GetCollidingBodies())
+		{
+			if (body is not Fruit fruit) continue;
+			if (Fuse(fruit)) break;
+		}
+    }
+
+	// TODO: Adjust weight of the fruit based on the size of the fruit
+    public void SetAttributes(FruitBuilder builder, FruitType type, CompressedTexture2D sprite, int scoreAmount, float radius, bool collided)
 	{
 		// Set variables
 		Type = type;
@@ -33,21 +58,27 @@ public partial class Fruit : RigidBody2D
 		this.sprite.Texture = sprite;
 		CircleShape2D circleShape = body.Shape as CircleShape2D;
 		circleShape.Radius = radius;
+		Collided = collided;
+
+		// Scale sprite to size of collision shape
+		float scaleFactor = radius * 2 / Math.Max(sprite.GetSize().X, sprite.GetSize().Y);
+		this.sprite.Scale = Vector2.One * scaleFactor;
 	}
 
-	public override void _Process(double delta)
+	private bool Fuse(Fruit otherFruit)
 	{
-	}
-
-	private void Fuse(Fruit otherFruit)
-	{
-		if (Type != otherFruit.Type || Type == FruitType.WATERMELON) return;
+		if (Type != otherFruit.Type || Type == FruitType.WATERMELON || IsQueuedForDeletion()) return false;
 		// Spawn new fruit between the two fruits
 		Vector2 middlePoint = (Position + otherFruit.Position) / 2;
-		builder.BuildFruit(Type + 1, middlePoint);
+		builder.BuildFruit(Type + 1, middlePoint, true);
+
+		// Emit signal for first collision if other fruit has not collided yet
+		if (otherFruit.Collided == false) otherFruit.EmitSignal(SignalName.FirstCollision);
+		GameLoop.Instance.AddScore(scoreAmount);
 
 		// Delete fruits involved in fusion
 		otherFruit.QueueFree();
 		QueueFree();
+		return true; // Indicate successful fusion
 	}
 }
