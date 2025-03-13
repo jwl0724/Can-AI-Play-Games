826bd8b612ba5ed9276cc5ca0e07dda4819985f3
jwl0724 <122506441+jwl0724@users.noreply.github.com>
2025-03-13 05:21:07 -0700

    implement box boundaries

--- BootlegSuika/suika-game-at-home/Scripts/Entities/Player.cs
+++ BootlegSuika/suika-game-at-home/Scripts/Entities/Player.cs
@@ -13,6 +13,7 @@ public partial class Player : Node2D
     // Running variables
     [Export] public float Speed { get; private set; } = 200;
     public bool IsHoldingFruit { get; private set; } = false;
+    private bool stop = false;
 
     // Signals
     [Signal] public delegate void DropFruitEventHandler();
@@ -26,28 +27,35 @@ public partial class Player : Node2D
         startPosition = Position;
 
         GameLoop.Instance.SetComponent(this);
+        GameLoop.Instance.Connect(GameLoop.SignalName.GameOver, Callable.From((int score) => OnGameOver()));
     }
 
     public override void _Process(double delta)
     {
+        if (stop) return;
         if (heldFruit != null) heldFruit.Position = fruitPosition.GlobalPosition;
     }
 
     public void Reset()
     {
+        stop = false;
         heldFruit = null;
         Position = startPosition;
     }
 
     public void Move(float direction, double delta)
     {
-        // TODO: Add box boundaries such that the fruit held can never go outside of the box
-        Position += Vector2.Right * direction * Speed * (float) delta;
+        if (stop) return;
+
+        Vector2 component = Vector2.Right * direction * Speed * (float) delta;
+        Position += component;
+
+        if (!IsInBounds()) Position -= component; // Undo move if went out of bounds
     }
 
     public void DropHeldFruit()
     {
-        if (heldFruit == null) return;
+        if (heldFruit == null || stop) return;
         heldFruit.Activate();
         heldFruit = null;
         IsHoldingFruit = false;
@@ -56,12 +64,37 @@ public partial class Player : Node2D
 
     public void GiveFruit(Fruit fruit)
     {
+        if (stop) return;
         IsHoldingFruit = true;
         heldFruit = fruit;
+
+        // Shift player back into area if newer fruit is bigger
+        float left = fruitPosition.GlobalPosition.X - heldFruit.Radius;
+        float right = fruitPosition.GlobalPosition.X + heldFruit.Radius;
+        float difference = 0;
+        if (right > GameLoop.Instance.BoxBorders.Y) difference = GameLoop.Instance.BoxBorders.Y - right - 2;
+        else if (left < GameLoop.Instance.BoxBorders.X) difference = GameLoop.Instance.BoxBorders.X - left + 2;
+        Position += Vector2.Right * difference;
     }
 
     public Vector2 GetFruitSpawnPoint()
     {
         return fruitPosition.GlobalPosition;
+    }
+
+    private void OnGameOver()
+    {
+        stop = true;
+        DropHeldFruit();
+    }
+
+    private bool IsInBounds()
+    {
+        if (heldFruit == null)
+            return Position.X > GameLoop.Instance.BoxBorders.X && Position.X < GameLoop.Instance.BoxBorders.Y + 40;
+
+        float fruitLeftSide = fruitPosition.GlobalPosition.X - heldFruit.Radius;
+        float fruitRightSide = fruitPosition.GlobalPosition.X + heldFruit.Radius;
+        return fruitLeftSide > GameLoop.Instance.BoxBorders.X && fruitRightSide < GameLoop.Instance.BoxBorders.Y;
     }
 }
