f89207bffcfb704418cff0dcdd32a373764f1db3
jwl0724 <122506441+jwl0724@users.noreply.github.com>
2025-03-28 19:56:45 -0700

    add back to menu button

--- BootlegSuika/suika-game-at-home/Scripts/UI/GameOverUI.cs
+++ BootlegSuika/suika-game-at-home/Scripts/UI/GameOverUI.cs
@@ -3,6 +3,7 @@ using Godot;
 public partial class GameOverUI : Control
 {
 	private Button retryButton;
+	private Button backButton;
 	private Label score;
 	private Vector2 defaultPosition;
 
@@ -11,12 +12,15 @@ public partial class GameOverUI : Control
 		defaultPosition = Position;
 		score = GetNode<Label>("Score");
 		retryButton = GetNode<Button>("Retry");
+		backButton = GetNode<Button>("Back");
 
 		Position = new Vector2(defaultPosition.X, -1000);
 		retryButton.Disabled = true;
 
 		retryButton.Connect("pressed", new Callable(this, nameof(OnRetry)));
 		(Owner as GameLoop).Connect(GameLoop.SignalName.GameOver, new Callable(this, nameof(OnGameOver)));
+
+		backButton.Connect("pressed", new Callable(this, nameof(OnBack)));
 	}
 
 	private void OnGameOver(int score)
@@ -36,5 +40,11 @@ public partial class GameOverUI : Control
 		hideTween.TweenProperty(this, nameof(Position).ToLower(), new Vector2(defaultPosition.X, 2000), 0.5);
 		hideTween.Play();
 		(Owner as GameLoop).ResetGame();
+	}
+
+	private void OnBack()
+	{
+		PackedScene mainMenu = ResourceLoader.Load<PackedScene>("res://Scenes/Levels/MainMenu.tscn");
+		GetTree().ChangeSceneToPacked(mainMenu);
 	}
 }
